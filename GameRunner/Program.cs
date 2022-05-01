64241cdb30fe6b2b301650db98500c0264e0ca3c
Alendon <Alexander@Familiepanick.de>
2022-05-01 14:36:49 +0200

    Initial Commit

---
+++ GameRunner/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using MintyCore;
+
+// ReSharper disable once RedundantAssignment
+bool debug = false;
+#if DEBUG
+debug = true;
+#endif
+
+var processFile = new FileInfo(Environment.ProcessPath!);
+
+var solutionFolder = processFile.Directory.Parent.Parent.Parent.Parent;
+var projectFolder = solutionFolder.EnumerateDirectories("Techardry", SearchOption.TopDirectoryOnly).FirstOrDefault();
+
+//Compile the project
+var compileProcess = new Process
+{
+    StartInfo = new ProcessStartInfo
+    {
+        FileName = "dotnet",
+        Arguments = $"build {projectFolder.FullName} -c {(debug ? "Debug" : "Release")}",
+        UseShellExecute = false,
+        RedirectStandardOutput = true,
+        RedirectStandardError = true,
+        CreateNoWindow = false,
+    }
+};
+compileProcess.ErrorDataReceived += (sender, e) =>
+{
+    if (e.Data != null)
+    {
+        Console.WriteLine(e.Data);
+    }
+};
+compileProcess.OutputDataReceived += (sender, e) =>
+{
+    if (e.Data != null)
+    {
+        Console.WriteLine(e.Data);
+    }
+};
+compileProcess.Start();
+compileProcess.BeginOutputReadLine();
+compileProcess.BeginErrorReadLine();
+compileProcess.WaitForExit();
+var exitCode = compileProcess.ExitCode;
+
+if (exitCode != 0)
+{
+    Console.WriteLine("Compilation failed");
+    Environment.Exit(exitCode);
+}
+
+var buildFolder = projectFolder.EnumerateDirectories("bin", SearchOption.TopDirectoryOnly).FirstOrDefault();
+buildFolder = buildFolder.EnumerateDirectories(debug ? "Debug" : "Release", SearchOption.TopDirectoryOnly)
+    .FirstOrDefault();
+var modFolder = buildFolder.EnumerateDirectories("net6.0", SearchOption.TopDirectoryOnly).FirstOrDefault();
+
+Engine.Main(new[] {$"-addModDir={modFolder.FullName}"});
---
+++ Techardry/TechardryMod.cs
@@ -0,0 +1,45 @@
+﻿using JetBrains.Annotations;
+using MintyCore.Modding;
+using MintyCore.Utils;
+
+namespace Techardry;
+
+[UsedImplicitly]
+public partial class TechardryMod : IMod
+{
+    public ushort ModId { get; set; }
+    public string StringIdentifier => "techardry";
+    public string ModDescription => "Techardry mod";
+    public string ModName => "Techardry";
+    public ModVersion ModVersion => new(0,0,1);
+    public ModDependency[] ModDependencies => Array.Empty<ModDependency>();
+    public GameType ExecutionSide => GameType.Local;
+    
+    public void Dispose()
+    {
+        Logger.WriteLog("Disposing TechardryMod", LogImportance.Info, "Techardry");
+    }
+
+    public void PreLoad()
+    {
+        Instance = this;
+    }
+
+    public void Load()
+    {
+        Logger.WriteLog("Loading TechardryMod", LogImportance.Info, "Techardry");
+        InternalRegister();
+    }
+
+    public void PostLoad()
+    {
+    }
+
+    public void Unload()
+    {
+        InternalUnregister();
+    }
+
+    public static TechardryMod? Instance { get; private set; }
+    
+}
