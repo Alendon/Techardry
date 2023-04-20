66f90bac90c8d9ab44440fac2765cfca82f06de9
Alendon <Alexander.Panick@live.de>
2023-04-20 10:59:05 +0200

    Added binning to testing bvh tree

--- Testing/BvhTest/BvhTree.cs
+++ Testing/BvhTest/BvhTree.cs
@@ -11,6 +11,7 @@ public class BvhTree
     private uint nodesUsed = 2;
 
     private const float FloatTolerance = 0.0001f;
+    private const int BinCount = 32;
 
     public BvhTree(Triangle[] triangles)
     {
@@ -37,13 +38,11 @@ public class BvhTree
     private void Subdivide(uint nodeIndex)
     {
         ref var node = ref _nodes[nodeIndex];
-        if (node.triangleCount <= 2) return;
-
-        var extent = node.Bounds.Max - node.Bounds.Min;
-        var axis = 0;
-        if (extent.Y > extent.X) axis = 1;
-        if (extent.Z > extent[axis]) axis = 2;
-        var splitPosition = node.Bounds.Min[axis] + extent[axis] * 0.5f;
+        
+        var splitCost = FindBestSplitPlane(ref node, out var axis, out var splitPosition);
+        var noSplitCost = CalculateNodeCost(ref node);
+        if(splitCost >= noSplitCost)
+            return;
 
         var i = node.leftFirst;
         var j = i + node.triangleCount - 1;
@@ -104,6 +103,102 @@ public class BvhTree
 
             BoundingBox.CreateMerged(BoundingBox.CreateFromPoints(points), node.Bounds, out node.Bounds);
         }
+    }
+
+    float FindBestSplitPlane(ref Node node, out int axis, out float splitPosition)
+    {
+        axis = -1;
+        splitPosition = float.NaN;
+        
+        float bestCost = float.MaxValue;
+
+        var bins = (stackalloc Bin[BinCount]);
+        var triangleVertices = new Vector3[3];
+
+        var leftArea = (stackalloc float[BinCount - 1]);
+        var rightArea = (stackalloc float[BinCount - 1]);
+        var leftCount = (stackalloc int[BinCount - 1]);
+        var rightCount = (stackalloc int[BinCount - 1]);
+
+        for (int a = 0; a < 3; a++)
+        {
+            var boundsMin = float.MaxValue;
+            var boundsMax = float.MinValue;
+
+            for (int i = 0; i < node.triangleCount; i++)
+            {
+                ref var triangle = ref _triangles[_triangleIndices[node.leftFirst + i]];
+                boundsMin = float.Min(boundsMin, triangle.Center[a]);
+                boundsMax = float.Max(boundsMax, triangle.Center[a]);
+            }
+            if(Math.Abs(boundsMin - boundsMax) < FloatTolerance) continue;
+            
+            bins.Clear();
+            
+            var scale = BinCount / (boundsMax - boundsMin);
+            for (int i = 0; i < node.triangleCount; i++)
+            {
+                ref var triangle = ref _triangles[_triangleIndices[node.leftFirst + i]];
+                var binIndex = int.Min(BinCount - 1, (int)((triangle.Center[a] - boundsMin) * scale));
+                bins[binIndex].Count++;
+                
+                triangleVertices[0] = triangle.V0;
+                triangleVertices[1] = triangle.V1;
+                triangleVertices[2] = triangle.V2;
+                
+                BoundingBox.CreateMerged(BoundingBox.CreateFromPoints(triangleVertices), bins[binIndex].Bounds, out bins[binIndex].Bounds);
+            }
+            
+            leftArea.Clear();
+            rightArea.Clear();
+            leftCount.Clear();
+            rightCount.Clear();
+            
+            BoundingBox leftBounds = default;
+            BoundingBox rightBounds = default;
+            int leftSum = 0, rightSum = 0;
+
+            for (int i = 0; i < BinCount -1; i++)
+            {
+                leftSum += bins[i].Count;
+                leftCount[i] = leftSum;
+                BoundingBox.CreateMerged(bins[i].Bounds, leftBounds, out leftBounds);
+                leftArea[i] = GetArea(leftBounds);
+                
+                rightSum += bins[BinCount - 1 - i].Count;
+                rightCount[BinCount - 2 - i] = rightSum;
+                BoundingBox.CreateMerged(bins[BinCount - 1 - i].Bounds, rightBounds, out rightBounds);
+                rightArea[BinCount - 2 - i] = GetArea(rightBounds);
+            }
+            
+            scale = (boundsMax - boundsMin) / BinCount;
+            for (int i = 0; i < BinCount -1; i++)
+            {
+                float planeCost = leftCount[i] * leftArea[i] + rightCount[i] * rightArea[i];
+                if (planeCost < bestCost)
+                {
+                    axis = a;
+                    splitPosition = boundsMin + (i + 1) * scale;
+                    bestCost = planeCost;
+                }
+
+            }
+
+        }
+
+        return bestCost;
+    }
+
+    float CalculateNodeCost(ref Node node)
+    {
+        var area = GetArea(node.Bounds);
+        return area * node.triangleCount;
+    }
+
+    private float GetArea(BoundingBox rightBounds)
+    {
+        var e = rightBounds.Max - rightBounds.Min;
+        return 2 * (e.X * e.Y + e.X * e.Z + e.Y * e.Z);
     }
 
     public void Intersect(ref Ray ray)
