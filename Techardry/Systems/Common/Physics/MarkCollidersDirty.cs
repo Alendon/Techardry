1337ace9e3abfac352cbcebf0763a90f723dde2a
Alendon <Alexander.Panick@live.de>
2024-05-11 21:45:35 +0200

    Adding physics components and system from engine

---
+++ Techardry/Systems/Common/Physics/MarkCollidersDirty.cs
@@ -0,0 +1,43 @@
+﻿using MintyCore.ECS;
+using MintyCore.Identifications;
+using MintyCore.Registries;
+using MintyCore.Utils;
+using Techardry.Components.Common.Physic;
+
+namespace Techardry.Systems.Common.Physics;
+
+//TODO change this to set colliders dynamically dirty
+/// <summary>
+///     System to mark colliders components dirty
+/// </summary>
+[RegisterSystem("mark_colliders_dirty")]
+[ExecuteInSystemGroup<PhysicSystemGroup>]
+[ExecuteAfter<CollisionSystem>]
+public partial class MarkCollidersDirty : ASystem
+{
+    [ComponentQuery] private readonly ComponentQuery<Collider> _componentQuery = new();
+
+    /// <inheritdoc />
+    public override Identification Identification => SystemIDs.MarkCollidersDirty;
+
+    /// <inheritdoc />
+    protected override void Execute()
+    {
+        if (World is null) return;
+        foreach (var entity in _componentQuery)
+        {
+            ref var collider = ref entity.GetCollider();
+            var bodyRef = World.PhysicsWorld.Simulation.Bodies.GetBodyReference(collider.Handle);
+
+            if (!bodyRef.Exists) continue;
+
+            collider.Dirty = true;
+        }
+    }
+
+    /// <inheritdoc />
+    public override void Setup(SystemManager systemManager)
+    {
+        _componentQuery.Setup(this);
+    }
+}
---
+++ Techardry/Systems/PhysicSystemGroup.cs
@@ -0,0 +1,37 @@
+﻿using MintyCore.ECS;
+using MintyCore.ECS.SystemGroups;
+using MintyCore.Identifications;
+using MintyCore.Physics;
+using MintyCore.Registries;
+using MintyCore.Utils;
+
+namespace Techardry.Systems;
+
+/// <summary>
+///     System group for physics
+/// </summary>
+[RegisterSystem("physic_group")]
+[ExecuteInSystemGroup<InitializationSystemGroup>]
+public class PhysicSystemGroup(IGameTimer timer) : ASystemGroup
+{
+    private float _accumulatedDeltaTime;
+
+    /// <inheritdoc />
+    public override Identification Identification => SystemIDs.PhysicGroup;
+    
+
+    /// <inheritdoc />
+    public override Task QueueSystem(IEnumerable<Task> dependency)
+    {
+        _accumulatedDeltaTime += timer.DeltaTime;
+        
+        var dependencies = dependency.ToList();
+        while (_accumulatedDeltaTime >= PhysicsWorld.FixedDeltaTime)
+        {
+            dependencies.Add(base.QueueSystem(dependencies));
+            _accumulatedDeltaTime -= PhysicsWorld.FixedDeltaTime;
+        }
+
+        return Task.WhenAll(dependencies);
+    }
+}
